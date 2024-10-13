b44ab068517ff29e644a7c23609f557c641a7681
rodentskie <rodentskie@gmail.com>
2024-10-13 16:36:02 +0800

    chore: about me

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,16 @@ module.exports = {
     extend: {
       animation: {
         scroll: "scroll 15s linear infinite",
+        "fade-in": "fadeIn 1.5s ease-in-out forwards",
       },
       keyframes: {
         scroll: {
           "0%": { transform: "translateX(100%)" },
           "100%": { transform: "translateX(-100%)" },
+        },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
       },
     },
